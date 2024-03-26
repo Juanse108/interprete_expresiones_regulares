a69ff9d7a6d115b2cd9d91ffcad64c7b53fad2e0
juansebastianmartinezjimenez <sebastiano805@hotmail.com>
2024-03-26 18:42:35 -0500

    expresion regular

--- models/expresion_regular.py
+++ models/expresion_regular.py
@@ -1,12 +1,26 @@
+import re
+
+
 class ExpresionRegular:
     def __init__(self, expresion_regular):
         self.expresion_regular = expresion_regular
 
     def get_expresion_regular(self):
         return self.expresion_regular
+
     def get_numero_estados(self):
-        """"""
+        # Implementación básica: cuenta los paréntesis como indicadores de estados
+        return self.expresion_regular.count("(") + self.expresion_regular.count(")")
+
     def get_numero_transiciones(self):
-        """"""
+        # Implementación básica: cuenta los símbolos de transición (excepto los meta-caracteres)
+        alfabeto = self.get_alfabeto()
+        return len(re.findall(f"[{re.escape(''.join(alfabeto))}]", self.expresion_regular))
+
     def get_alfabeto(self):
-        """"""
+        # Implementación básica: extrae los caracteres individuales, excluyendo meta-caracteres
+        alfabeto = set()
+        for caracter in self.expresion_regular:
+            if caracter.isalnum():
+                alfabeto.add(caracter)
+        return alfabeto
